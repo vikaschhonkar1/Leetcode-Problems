d128e33550b7055cb616e1e26b459d662d127acf
Vikas Chhonkar <66249172+vikaschhonkar1@users.noreply.github.com>
2022-07-04 15:57:32 +0530

    Time: 4 ms (70.03%), Space: 52.4 MB (20.56%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        if(n==0) return 0;
+        
+        int left[] = new int[n];
+        for(int i=0; i<n; i++)  left[i] = 1;
+        
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1] + 1;
+            }
+        }
+        
+        int right[] = new int[n];
+        for(int i=0; i<n; i++)  right[i] = 1;
+        
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                right[i] = right[i+1] + 1;
+            }
+        }
+        
+//         for(int i=0; i<n; i++){
+//             System.out.print(left[i]+" ");
+//         }
+//         System.out.println();
+        
+//         for(int i=0; i<n; i++){
+//             System.out.print(right[i]+" ");
+//         }
+//         System.out.println();
+        int count = 0;
+        for(int i=0; i<n; i++){
+            count += Math.max(left[i], right[i]);
+        }
+        return count;
+    }
+}
