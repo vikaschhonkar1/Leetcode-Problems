f74b57b8ff0ac94d5bc27d8abe2342ebd0fc2bf5
Vikas Chhonkar <66249172+vikaschhonkar1@users.noreply.github.com>
2022-08-14 19:52:38 +0530

    Time: 1 ms (99.91%), Space: 55 MB (50.73%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -2,9 +2,9 @@ class Solution {
     public void reverseString(char[] s) {
         int n = s.length;
         for(int i=0; i<n/2; i++){
-            char ch = s[i];
+            char temp = s[i];
             s[i] = s[n-i-1];
-            s[n-i-1] = ch;
+            s[n-i-1] = temp;
         }
     }
 }
