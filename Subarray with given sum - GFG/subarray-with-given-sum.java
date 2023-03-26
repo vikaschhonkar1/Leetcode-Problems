43fa8046f3af007227e0ec1e818f834c7b0f935f
Vikas Chhonkar <66249172+vikaschhonkar1@users.noreply.github.com>
2023-03-26 14:20:44 +0530

    Added solution - LeetHub

--- Subarray with given sum - GFG/subarray-with-given-sum.java
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -1,32 +1,39 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 class Main{
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
 
-        int t = sc.nextInt();
+        int t = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < t; i++) {
-            int n = sc.nextInt();
-            int s = sc.nextInt();
-
-            int[] m = new int[n];
-            for (int j = 0; j < n; j++) {
-                m[j] = sc.nextInt();
-            }
+        while(t-->0){
             
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
             Solution obj = new Solution();
-            ArrayList<Integer> res = obj.subarraySum(m, n, s);
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
             for(int ii = 0;ii<res.size();ii++)
-                System.out.print(res.get(ii) + " ");
-            System.out.println();
+                ot.print(res.get(ii) + " ");
+            ot.println();
         }
+        ot.close();
     }
 
-}// } Driver Code Ends
+}
+// } Driver Code Ends
 
 
 class Solution
@@ -34,30 +41,33 @@ class Solution
     //Function to find a continuous sub-array which adds up to a given number.
     static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
     {
-        // Your code here
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        int currentSum = arr[0];
-        int first = 0;
-        for(int i=1; i<=n; i++){
+        // List for storing and returing answer
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        
+        int i = 0;
+        int j = 0;
+        int tempSum = 0;
+        
+        while(i<n && j<n){
+            tempSum += arr[j];
             
-            while(currentSum>s && first < i-1){
-                currentSum-=arr[first];
-                first++;
-            }
-            if(currentSum == s){
-                list.add(first+1);
-                list.add(i);
-                return list;
-            }
-            if(i<n){
-                currentSum += arr[i];
-            }
+            if(tempSum==s && i<=j){
+                ans.add(i+1);
+                ans.add(j+1);
+                return ans;
+            } 
             
-        }
-        if(list.size()==0){
-            list.add(-1);
+            if(tempSum<s){
+                j++;
+            } else {
+                tempSum -= arr[i];
+                tempSum -= arr[j];
+                i++;
+            }
         }
         
-        return list;
+        
+        if(ans.size()==0) ans.add(-1);
+        return ans;
     }
 }
