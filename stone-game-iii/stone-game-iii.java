011c6895e61551c9df2e45226f91b18485781c79
Vikas Chhonkar <66249172+vikaschhonkar1@users.noreply.github.com>
2023-05-27 07:31:05 +0530

    Time: 20 ms (53.27%) | Memory: 55.5 MB (45.23%) - LeetSync

--- stone-game-iii/stone-game-iii.java
+++ stone-game-iii/stone-game-iii.java
@@ -1,11 +1,11 @@
 class Solution {
     public String stoneGameIII(int[] A) {
-        int n = A.length, dp[] = new int[n+1];
+        int n = A.length, dp[] = new int[4];
         for (int i = n - 1; i >= 0; --i) {
-            dp[i] = Integer.MIN_VALUE;
+            dp[i % 4] = Integer.MIN_VALUE;
             for (int k = 0, take = 0; k < 3 && i + k < n; ++k) {
                 take += A[i + k];
-                dp[i] = Math.max(dp[i], take - dp[i + k + 1]);
+                dp[i % 4] = Math.max(dp[i % 4], take - dp[(i + k + 1) % 4]);
             }
         }
         if (dp[0] > 0) return "Alice";
