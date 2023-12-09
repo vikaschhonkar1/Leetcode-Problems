279cc1ef2a56364acc51165b7a79a8e127dec11f
Vikas Chhonkar <66249172+vikaschhonkar1@users.noreply.github.com>
2023-12-09 17:18:19 +0530

    Time: 1 ms (98.35%) | Memory: 44.6 MB (23.99%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -14,32 +14,25 @@ class Solution {
             return false;
         }
 
-        int canPlant1 = 0;
-        int canPlant2 = 0;
+        int canPlant = 0;
 
         int tempArray1[] = flowerbed;
-        int tempArray2[] = flowerbed;
 
         if(tempArray1[0] == 0 && tempArray1[1] == 0) {
             tempArray1[0] = 1;
-            canPlant1++;
+            canPlant++;
         }
         for(int i = 1; i < size - 1; i++) {
             if(tempArray1[i] == 0 && tempArray1[i - 1] == 0 && tempArray1[i + 1] == 0) {
                 tempArray1[i] = 1;
-                canPlant1++;
+                canPlant++;
             } 
         }
         if(tempArray1[size - 1] == 0 && tempArray1[size - 2] == 0) {
             tempArray1[size - 1] = 1;
-            canPlant1++;
+            canPlant++;
         }
 
-        for(int i = 0; i < size; i++) {
-            System.out.print(tempArray1[i] + " ");
-        }
-
-        int canPlant = Math.max(canPlant1, canPlant2);
         if(canPlant >= n) return true;
         return false;
     }
