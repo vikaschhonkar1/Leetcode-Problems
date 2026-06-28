a8511e4fe0a5a344b2503135e165a67cd79b87a6
Vikas Chhonkar <66249172+vikaschhonkar1@users.noreply.github.com>
2026-06-28 15:56:32 +0530

    Time: 211 ms (13.46%) | Memory: 46.5 MB (14.43%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,31 +1,31 @@
 class Solution {
+
     private boolean dfs(int r, int c, int idx, char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
 
-        if (r < 0 || r >= m || c < 0 || c >= n) {
-            return false;
-        }
+        // Rule out out-of-bound indexes
+        if (r < 0 || r >= m || c < 0 || c >= n) return false;
 
-        if (board[r][c] != word.charAt(idx)) {
-            return false;
-        }
+        // If char dont match word's char
+        if (board[r][c] != word.charAt(idx)) return false;
 
-        if (idx == word.length() - 1) {
-            return true;
-        }
+        // now if the index is last one and from above it is sure that the elements match 
+        if (idx == word.length() - 1) return true;
 
+        // Store current value in temp and mark it some another value so that we do not 
+        // count it multiple times in dfs
         char temp = board[r][c];
         board[r][c] = '*';
 
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
 
         for (int d = 0; d < 4; d++) {
             int nr = r + dx[d];
             int nc = c + dy[d];
 
-            if (dfs(nr, nc, idx + 1, board, word)) {
+            if (dfs (nr, nc, idx + 1, board, word)) {
                 board[r][c] = temp;
                 return true;
             }
@@ -36,13 +36,17 @@ class Solution {
     }
 
 
+
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
 
-        for (int i = 0 ; i < m; i++) {
+        // Traverse each element and find whether we can start dfs from there or not 
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == word.charAt(0)) {
+                    // DFS state will look like
+                    // current_i, current_j, current_idx_of_word, board, word)
                     if (dfs(i, j, 0, board, word)) {
                         return true;
                     }
